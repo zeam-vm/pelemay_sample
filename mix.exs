1e392b95311a4f35cce5e1b6e462a3eded10e484
Susumu Yamazaki <zacky1972@gmail.com>
2021-01-10 09:31:19 +0900

    version 0.0.15

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule PelemaySample.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:pelemay, "~> 0.0.14"},
+      {:pelemay, "~> 0.0.15"},
 
       # Benchmark dependencies
       {:benchfella, "~> 0.3.5"},
